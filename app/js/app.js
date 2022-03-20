89818fec1c85f8f21048d5b49735b1b53a32133e
serdjukow <sergej@serdjukow.eu>
2022-03-20 19:54:32 +0100

    U-2

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	const headerSticky = () => {
 		const wrapper = document.querySelector('body')
 		const navbar = document.querySelector('.navbar')
-		wrapper.addEventListener('scroll', () => (wrapper.scrollTop >= 200 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky')))
+		wrapper.addEventListener('scroll', () => (wrapper.scrollTop >= 300 ? navbar.classList.add('sticky') : navbar.classList.remove('sticky')))
 	}
 	headerSticky()
 })
